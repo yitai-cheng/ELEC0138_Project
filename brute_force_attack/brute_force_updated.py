af6cc45ad5402883d19ef0ab90ece0dd8ac78c87
Rares00 <zceeran@ucl.ac.uk>
2023-04-21 12:32:28 +0100

    update architecture

--- brute_force_attack/brute_force_updated.py
+++ brute_force_attack/brute_force_updated.py
@@ -1,19 +1,18 @@
 #program that brute forces a password
-
 import itertools
 import string
 import time
 
 #password for testing
 
-test_password = "password"
+test_password = "ba12"
 
-#load the data from 1000-most-common-passwords.txt and dictionary.txt into a list
+#load the data 
 
-with open("1000-most-common-passwords.txt", "r") as f:
+with open("/Users/raresnitu/Documents/security_project/ELEC0138_Project/brute_force_attack/1000-most-common-passwords.txt", "r") as f:
     common_passwords = f.read().splitlines()
 
-with open("dictionary.txt", "r") as f:
+with open("/Users/raresnitu/Documents/security_project/ELEC0138_Project/brute_force_attack/dictionary.txt", "r") as f:
     english_dictionary = f.read().splitlines()
 
 
@@ -21,12 +20,12 @@ with open("dictionary.txt", "r") as f:
 
 possible_characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 
-#time the program to see how long it takes to crack the password
+#time the program
 #start the timer
 
 start_time = time.time()
 
-#generate all possible combinations of characters   
+#test if the password is in the list of common passwords or the dictionary
 if test_password in common_passwords:
     password = test_password
     end_time = time.time()
@@ -39,7 +38,7 @@ elif test_password in english_dictionary:
     print("Password cracked: " + password)
     print("Time taken: " + str(end_time - start_time) + " seconds")
     print("Password found in dictionary")
-
+#generate all possible combinations of characters 
 else:
     print("Password not found in common passwords or dictionary")
     for lenght in range(1, len(test_password)+1):
